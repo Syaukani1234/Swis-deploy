2d9e7e399645acd74e3207f83744004927c5fc26
Syau Kani <kanis5725@gmail.com>
2024-08-02 14:56:52 +0000

    s5

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xea65D3f80d2253293a64e2f8DdF4CEC401823053'
-export const ProxyAdmin = '0x6a0baE5CF96d14e39FA9F5AdB4fadABb55B22E17'
-export const SWTRImplementation = '0xE4e93a474734E22A4f660EF1fE7aC3fc096E856c'
+export const SWTRProxy = '0x45cEF717536E4059094Cf4120C71825FE68e84a6'
+export const ProxyAdmin = '0xdbAb5201e02bBB4D02E05BA7F29b9a859F8Ee6A9'
+export const SWTRImplementation = '0xd0D4a29f3f60f14e9ABd52Ecb3f125BeCe1f19bF'
     
